7dc3748568e8d7ae3dcdb851dec171fb6d4bc700
Nizam uddin Sangram <nizamuddinstruggle@gmail.com>
2024-06-01 15:40:42 +0600

    find a user role api

---
+++ index.js
@@ -0,0 +1,87 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vshvqji.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const usersCollection = client.db("task-rabbit").collection("users");
+    // when a user register a account give point and store user data
+    app.post("/register", async (req, res) => {
+      const { name, email, role, image_url } = req.body;
+      const existingUser = await usersCollection.findOne({ email });
+      if (existingUser) {
+        return res.status(400).send({ message: "Email already in use" });
+      }
+      const initialCoins = role === "Worker" ? 10 : 50;
+      const newUser = {
+        name,
+        email,
+        role,
+        image_url,
+        coins: initialCoins,
+      };
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+    //when user can entry google account
+    app.post("/google-login", async (req, res) => {
+      const { name, email, image_url } = req.body;
+      const isExit = await usersCollection.findOne({ email });
+      if (isExit) {
+        return res.send({
+          message: "User already exists, logged in successfully",
+        });
+      }
+      const newUser = {
+        name,
+        email,
+        role: "Worker",
+        image_url,
+        coins: 10,
+      };
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+    //find user role from database
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email });
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("task rabbit server started");
+});
+app.listen(port, () => {
+  console.log(`running rabbit server on port ${port}`);
+});
